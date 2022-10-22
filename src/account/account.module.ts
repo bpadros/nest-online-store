a019bfd731af402a95c9a7b27ccc74e99fb50957
bpadros <bpadros@gmail.com>
2022-10-22 19:23:15 -0300

    new nest project

---
+++ src/account/account.module.ts
@@ -0,0 +1,8 @@
+/* eslint-disable prettier/prettier */
+import { Module } from '@nestjs/common';
+import { AccountController } from './account.controller';
+
+@Module({
+  controllers: [AccountController],
+})
+export class AccountModule {}
